fe2e59e654e8845ec0a82bbd4bb89e60a3b97de1
GT <georgetiser@gmail.com>
2015-05-12 13:15:13 -0400

    This version fixes the missing navigation problem. It has the chapters in the original order. We intend to test and verify that all videos work properly, then swap chapters 1 and 2.

--- config.lua
+++ config.lua
@@ -8,4 +8,11 @@ application =
           fps = 60, 
           scale = "letterbox"
      }, 
+		 license =
+		 {
+			google =
+			{
+				key = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArPaoJN5qGgBRCTxDuFevyj9DoKOG2XmXS+79QjFgbHcyMn5SbYXAoR5TJ6IuoHEZ/VsLpA3leoLBjzsbHJyDv4zhUW2V90fE4pOgG+xH6qteYph+RZxJ1NzSCxVcmisET4vbYqkMynnKIUXXV8bcBPSw3RWanFV7sO7nfIh1QFyoUCbxhtArvshhFyF81qdg7thg510MXxtTWkct4sSlQCuRpIUDsJFLdcpJWsQ8MgV+SJ/GLyho8zCnehuXC0aDP+WbS/pXn6C7lZV9nFk5VP+fhgUh3N6jOvUvfNLdlEj8+O8BWX1j5VxbCJ8GKdw4sS58Er12P8SKxG0EbWIP4wIDAQAB"
+			}
+		 }
 } 
