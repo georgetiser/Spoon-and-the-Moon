313f931ce608d1901c5792b5cd8c97c80a06c69e
George Tiser <georgetiser@gmail.com>
2015-08-27 16:00:11 -0400

    This intermediate commit stores some early-stage file cleanup, removing an incidental use of the word 'toggle' is the only real change.

--- book1/gitback.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Restoring files that were deleted since last commit.
-git ls-files --deleted > tempfiles.txt
-FOR /F "usebackq delims=" %%G IN (tempfiles.txt) DO (echo %%G & git checkout %%G)
-del tempfiles.txt
-exit
